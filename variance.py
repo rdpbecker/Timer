162cf699fdf77324bf52424597e3369a01430134
Riley <rileydbecker@gmail.com>
2019-09-18 22:24:41 -0400

    Start the script

--- variance.py
+++ variance.py
@@ -1,10 +1,36 @@
-def getSplitNames(self):
+def getSplitNames():
     splitNames = cate.findAllSplits()
     names = cate.findNames(splitNames,0)
-    self.game = cate.readThingInList(names)
-    cate.restrictCategories(splitNames,self.game)
+    game = cate.readThingInList(names)
+    cate.restrictCategories(splitNames,game)
     categories = cate.findNames(splitNames,1)
-    self.category = cate.readThingInList(categories)
-    self.splitnames = cate.findGameSplits(splitNames,self.category)
-    fileio.stripEmptyStrings(self.splitnames)
+    category = cate.readThingInList(categories)
+    splitnames = cate.findGameSplits(splitNames,category)
+    fileio.stripEmptyStrings(splitnames)
+    return [game,category,splitnames]
 
+def cutStart(csv):
+  for i in range(len(csv)):
+    csv[i] = csv[i][7:]
+
+def filterEven(csv):
+  new = []
+  for i in range(len(csv)):
+    line = []
+    for j in range(len(csv[i])):
+      if not j%2:
+        line.append(csv[i][j])
+    new.append(line)
+  return new
+
+def main():
+  [game,category,splitnames] = getSplitNames()
+  completeCsv = fileio.csvReadStart(game,category,splitnames)
+  completeCsv = completeCsv[1:]
+  cutStart(completeCsv)
+  completeCsv = filterEven(completeCsv)
+
+if __name__ == "__main__":
+  import categorySelection as cate
+  import fileio
+  main()
