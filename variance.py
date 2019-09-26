0b3e50c877fa76d069dbac98299ad87dcde38e83
Riley <rileydbecker@gmail.com>
2019-09-25 20:24:24 -0400

    Complete the script

--- variance.py
+++ variance.py
@@ -1,3 +1,9 @@
+global varianceList
+
+def printLines(lines):
+  for line in lines:
+    print line.__str__()
+
 def getSplitNames():
     splitNames = cate.findAllSplits()
     names = cate.findNames(splitNames,0)
@@ -23,14 +29,43 @@ def filterEven(csv):
     new.append(line)
   return new
 
+def getRows(completeCsv):
+  rows = []
+  for i in range(len(completeCsv)):
+    timeList = Timelist.Timelist()
+    for j in range(len(completeCsv[i])):
+      timeList.insert(Time.Time(5,timestring=completeCsv[i][j]))
+    rows.append(timeList)
+  return rows
+
+def sort(i):
+  return varianceList[i]
+
 def main():
+  global varianceList
   [game,category,splitnames] = getSplitNames()
   completeCsv = fileio.csvReadStart(game,category,splitnames)
   completeCsv = completeCsv[1:]
   cutStart(completeCsv)
   completeCsv = filterEven(completeCsv)
+  timeRows = getRows(completeCsv)
+  varianceList = []
+  for row in timeRows:
+    avg = row.average().toSecs()
+    secsList = [x.toSecs() for x in row.timelist]
+    filtered = filter(lambda x: not x == 0,secsList)
+    varList = [(x-avg)**2 for x in filtered]
+    variance = sum(varList)/len(varList)
+    varianceList.append(100*variance/avg)
+  print "In Order:\n\n"
+  printLines(splitnames[i]+": "+'%.3f'%(varianceList[i])+"%" for i in range(len(varianceList)))
+
+  print "\n\n\nSorted:\n\n"
+  sortedRange = sorted(range(len(varianceList)),key=sort,reverse=True)
+  printLines(splitnames[i]+": "+'%.3f'%(varianceList[i])+"%" for i in sortedRange) 
 
 if __name__ == "__main__":
   import categorySelection as cate
   import fileio
+  import Time, Timelist
   main()
