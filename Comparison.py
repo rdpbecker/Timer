57d317c6755f3f1dc69441521050dedcf1e59cdc
Riley Becker <rileydbecker@gmail.com>
2021-03-29 20:31:57 -0400

    Update the Comparison object to include the diffs.

--- Comparison.py
+++ Comparison.py
@@ -3,12 +3,16 @@ class Comparison:
     totalHeader = ""
     segments = []
     totals = []
+    segmentDiffs = []
+    totalDiffs = []
 
-    def __init__(self,shead,thead,segments,totals):
+    def __init__(self,shead,thead,segments,totals,segmentDiffs,totalDiffs):
         self.segmentHeader = shead
         self.totalHeader = thead
         self.segments = segments
         self.totals = totals
+        self.segmentDiffs = segmentDiffs
+        self.totalDiffs = totalDiffs
 
     def getSegmentHeader(self):
         return self.segmentHeader
@@ -21,6 +25,12 @@ class Comparison:
 
     def getTotals(self):
         return self.totals
+
+    def getSegmentDiffs(self):
+        return self.segmentDiffs
+
+    def getTotalDiffs(self):
+        return self.totalDiffs
 
     def getSegment(self,index):
         return self.segments[index]
