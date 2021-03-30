e173177e0787780ba0903cf4c7b3d343829abad8
Riley Becker <rileydbecker@gmail.com>
2021-03-29 20:11:13 -0400

    Fix an issue where the stringToTime method fails if the time isn't at least an hour.

--- timeHelpers.py
+++ timeHelpers.py
@@ -36,13 +36,16 @@ def toString(totalSecs,flag=0,flag2=1,precision=0):
         string = string + str(fracsecs)[1:precision+2]
     return string
 
-def stringToTime(timestring)
+def stringToTime(timestring):
     if timestring == "-":
         return "BLANK"
     parts1 = re.split("\.",timestring)
     parts2 = re.split(":",parts1[0])
-    if precision and len(parts1) > 1:
-        stringPrecision = len(parts1[1])
+    hours = 0
+    mins = 0
+    secs = 0
+    fracsecs = 0
+    if len(parts1) > 1:
         fracsecs = float("0."+parts1[1])
     if len(parts2) == 3:
         hours = int(parts2[0])
