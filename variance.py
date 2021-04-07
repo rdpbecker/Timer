e396dac96ec7a6105c54a4ae5e4ae5b343301ee8
Riley Becker <rileydbecker@gmail.com>
2021-04-06 20:37:01 -0400

    Fix the variance script to use the updated versions of all the utility functions that got updated in the last PR. Also move the config stuff into fileio and the getSplitNames function into the category selection module.

--- variance.py
+++ variance.py
@@ -1,11 +1,11 @@
 global varianceList
 
 def printLines(lines):
-  for line in lines:
-    print(line.__str__())
+    for line in lines:
+        print(line)
 
-def getSplitNames():
-    splitNames = cate.findAllSplits()
+def getSplitNames(baseDir):
+    splitNames = cate.findAllSplits(baseDir)
     names = cate.findNames(splitNames,0)
     game = cate.readThingInList(names)
     cate.restrictCategories(splitNames,game)
@@ -16,56 +16,57 @@ def getSplitNames():
     return [game,category,splitnames]
 
 def cutStart(csv):
-  for i in range(len(csv)):
-    csv[i] = csv[i][7:]
+    for i in range(len(csv)):
+        csv[i] = csv[i][1:]
 
 def filterEven(csv):
-  new = []
-  for i in range(len(csv)):
-    line = []
-    for j in range(len(csv[i])):
-      if not j%2:
-        line.append(csv[i][j])
-    new.append(line)
-  return new
+    new = []
+    for i in range(len(csv)):
+        line = []
+        for j in range(len(csv[i])):
+            if not j%2:
+                line.append(csv[i][j])
+        new.append(line)
+    return new
 
 def getRows(completeCsv):
-  rows = []
-  for i in range(len(completeCsv)):
-    timeList = Timelist.Timelist()
-    for j in range(len(completeCsv[i])):
-      timeList.insert(Time.Time(5,timestring=completeCsv[i][j]))
-    rows.append(timeList)
-  return rows
+    rows = []
+    for i in range(len(completeCsv)):
+        timeList = []
+        for j in range(len(completeCsv[i])):
+            time = timeh.stringToTime(completeCsv[i][j])
+            if not timeh.isBlank(time):
+                timeList.append(time)
+        rows.append(timeList)
+    return rows
 
 def sort(i):
-  return varianceList[i]
+    return varianceList[i]
 
 def main():
-  global varianceList
-  [game,category,splitnames] = getSplitNames()
-  completeCsv = fileio.csvReadStart(game,category,splitnames)
-  completeCsv = completeCsv[1:]
-  cutStart(completeCsv)
-  completeCsv = filterEven(completeCsv)
-  timeRows = getRows(completeCsv)
-  varianceList = []
-  for row in timeRows:
-    avg = row.average().toSecs()
-    secsList = [x.toSecs() for x in row.timelist]
-    filtered = [x for x in secsList if not x == 0]
-    varList = [(x-avg)**2 for x in filtered]
-    variance = sum(varList)/len(varList)
-    varianceList.append(100*variance/avg)
-  print("In Order:\n\n")
-  printLines(splitnames[i]+": "+'%.3f'%(varianceList[i])+"%" for i in range(len(varianceList)))
+    global varianceList
+    config = fileio.getUserConfig()
+    [game,category,splitnames] = getSplitNames(config["baseDir"])
+    completeCsv = fileio.csvReadStart(config["baseDir"],game,category,splitnames)[0]
+    completeCsv = completeCsv[1:]
+    cutStart(completeCsv)
+    completeCsv = filterEven(completeCsv)
+    timeRows = getRows(completeCsv)
+    varianceList = []
+    for row in timeRows:
+        avg = sum(row)/len(row)
+        varList = [(x-avg)**2 for x in row]
+        variance = sum(varList)/len(varList)
+        varianceList.append(100*variance/avg)
+    print("In Order:\n\n")
+    printLines(splitnames[i]+": "+'%.3f'%(varianceList[i])+"%" for i in range(len(varianceList)))
 
-  print("\n\n\nSorted:\n\n")
-  sortedRange = sorted(list(range(len(varianceList))),key=sort,reverse=True)
-  printLines(splitnames[i]+": "+'%.3f'%(varianceList[i])+"%" for i in sortedRange) 
+    print("\n\n\nSorted:\n\n")
+    sortedRange = sorted(list(range(len(varianceList))),key=sort,reverse=True)
+    printLines(splitnames[i]+": "+'%.3f'%(varianceList[i])+"%" for i in sortedRange)
 
 if __name__ == "__main__":
-  import categorySelection as cate
-  import fileio
-  import Time, Timelist
-  main()
+    import categorySelection as cate
+    import fileio
+    import timeHelpers as timeh
+    main()
