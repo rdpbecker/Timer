0cdaa32026411e39c53c30223abf388863fff510
Riley Becker <rileydbecker@gmail.com>
2021-04-14 11:45:48 -0400

    Add the padding to the config so that it's easier to make it consistent.

--- Components/CompareName.py
+++ Components/CompareName.py
@@ -11,8 +11,8 @@ class CompareName(Component.Component):
         self.state = state
         self.header = tk.Label(self, text="Comparing Against:", fg=state.config["root"]["colours"]["text"], bg=state.config["root"]["colours"]["bg"])
         self.compare = tk.Label(self, text=state.currentComparison.totalHeader, fg=state.config["root"]["colours"]["text"], bg=state.config["root"]["colours"]["bg"])
-        self.header.grid(row=0,column=0,columnspan=8,sticky='W',ipadx=10)
-        self.compare.grid(row=0,column=8,columnspan=4,sticky='E',ipadx=10)
+        self.header.grid(row=0,column=0,columnspan=8,sticky='W',ipadx=state.config["padx"])
+        self.compare.grid(row=0,column=8,columnspan=4,sticky='E',ipadx=state.config["padx"])
 
     def onComparisonChanged(self):
         self.compare.configure(text=self.state.currentComparison.totalHeader)
