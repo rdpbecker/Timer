5beee2dfd7a0f82fa54df1d4db383a6126fa9e00
Riley Becker <rileydbecker@gmail.com>
2021-04-13 23:30:01 -0400

    Add a CompareName component which displays the name of the current comparison. Updates when the comparison is changed.

---
+++ Components/CompareName.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+from Components import Component
+
+class CompareName(Component.Component):
+    header = ""
+    compare = ""
+
+    def __init__(self,parent,state):
+        Component.Component.__init__(self,parent,state)
+        self.configure(bg=state.config["root"]["colours"]["bg"])
+        self.state = state
+        self.header = tk.Label(self, text="Comparing Against:", fg=state.config["root"]["colours"]["text"], bg=state.config["root"]["colours"]["bg"])
+        self.compare = tk.Label(self, text=state.currentComparison.totalHeader, fg=state.config["root"]["colours"]["text"], bg=state.config["root"]["colours"]["bg"])
+        self.header.grid(row=0,column=0,columnspan=8,sticky='W')
+        self.compare.grid(row=0,column=8,columnspan=4,sticky='E')
+
+    def onComparisonChanged(self):
+        self.compare.configure(text=self.state.currentComparison.totalHeader)
