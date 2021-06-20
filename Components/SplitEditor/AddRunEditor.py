ca3e118cd0e9170df2ac3d25cd9c8372f4dd2611
Riley Becker <rileydbecker@gmail.com>
2021-06-19 22:31:02 -0400

    Move the main portion of the editor into a component common to both types of editors.

--- Components/SplitEditor/AddRunEditor.py
+++ Components/SplitEditor/AddRunEditor.py
@@ -1,9 +1,8 @@
 import tkinter as tk
 from DataClasses import AllSplitNames
-from Dialogs import Popup
 from Components.SplitEditor import EntryGrid
+from Components.SplitEditor import MainEditor
 from Components import GameSelector
-from Components import SaveButton
 from util import fileio
 from util import dataManip
 from util import readConfig as rc
@@ -11,9 +10,8 @@ from util import readConfig as rc
 class SplitEditor(tk.Frame):
     def __init__(self,master):
         super().__init__(master)
-        self.localComparisons = dataManip.newComparisons()
-        self.splits = AllSplitNames.Splits()
         self.config = rc.getUserConfig()
+        self.splits = AllSplitNames.Splits()
 
         self.selection = GameSelector.Selector(self)
         self.selection.pack(side="top",fill="x")
@@ -21,78 +19,36 @@ class SplitEditor(tk.Frame):
         self.oldGame = ""
         self.oldCategory = ""
 
-        self.buttonFrame = tk.Frame(self)
-        self.buttonFrame.pack(side="right",fill="y")
-
-        self.entries = EntryGrid.EntryGrid(self,self.localComparisons)
-        self.entries.pack(side="left")
-
-        self.addSplitButton = tk.Button(self.buttonFrame, text="Add Split", command=self.addSplit)
-        self.addSplitButton.pack(fill="x")
-
-        self.deleteSplitButton = tk.Button(self.buttonFrame, text="Delete Split", command=self.deleteSplit)
-        self.deleteSplitButton.pack(fill="x")
-        if not len(self.entries.rows):
-            self.deleteSplitButton["state"] = "disabled"
-
-        self.addComparisonButton = tk.Button(self.buttonFrame, text="Add Comparison", command=self.addComparison)
-        self.addComparisonButton.pack(fill="x")
-
-        self.deleteComparisonButton = tk.Button(self.buttonFrame, text="Delete Comparison", command=self.deleteComparison)
-        self.deleteComparisonButton.pack(fill="x")
-        if len(self.entries.comparisons) <= 5:
-            self.deleteComparisonButton["state"] = "disabled"
-
-        self.saveButton = SaveButton.SaveButton(self.buttonFrame, \
-            {"callback": self.save, \
-            "valid": self.validSave, \
-            "invalidMsg": "Runs must have a\nnon-empty game and\ncategory"})
-        self.saveButton.pack(side="bottom",fill="x")
-        self.saveWarning = tk.Label(self.buttonFrame,text="Warning: some\ncurrent values are\ninvalid. For invalid\nvalues, the most\nrecent valid value\n will be saved.",fg="orange")
+        self.editor = MainEditor.Editor(self,dataManip.newComparisons())
+        self.editor.pack(side="bottom")
+        self.editor.saveButton.options["callback"] = self.save
+        self.editor.saveButton.options["valid"] = self.validSave
 
     def updateRows(self,*args):
-        self.entries.pack_forget()
+        self.editor.entries.pack_forget()
         if not self.splits.validPair(self.selection.game,self.selection.category):
             if not self.splits.validPair(self.oldGame,self.oldCategory):
-                self.entries.pack(side="left")
+                self.editor.entries.pack(side="left")
                 return
-            self.entries = EntryGrid.EntryGrid(self,dataManip.newComparisons())
+            self.editor.entries = EntryGrid.EntryGrid(self.editor,dataManip.newComparisons())
         else:
-            self.entries = EntryGrid.EntryGrid(self,fileio.csvReadStart(self.config["baseDir"],self.selection.game,self.selection.category,self.splits.getSplitNames(self.selection.game,self.selection.category))[1])
-        self.entries.pack(side="left")
+            self.editor.entries = EntryGrid.EntryGrid(self.editor,fileio.csvReadStart(self.config["baseDir"],self.selection.game,self.selection.category,self.splits.getSplitNames(self.selection.game,self.selection.category))[1])
+        self.editor.entries.pack(side="left")
         self.oldGame = self.selection.game
         self.oldCategory = self.selection.category
 
-    def addSplit(self,event=None):
-        self.entries.addSplit()
-        self.deleteSplitButton["state"] = "active"
-
-    def deleteSplit(self,event=None):
-        self.entries.removeSplit()
-        if not len(self.entries.rows):
-            self.deleteSplitButton["state"] = "disabled"
-
-    def addComparison(self,event=None):
-        self.entries.addComparison()
-        self.deleteComparisonButton["state"] = "active"
-
-    def deleteComparison(self,event=None):
-        self.entries.removeComparison()
-        if len(self.entries.comparisons) <= 5:
-            self.deleteComparisonButton["state"] = "disabled"
-
     def validSave(self):
-        self.saveWarning.pack_forget()
+        self.editor.saveWarning.pack_forget()
         if not (self.selection.game and self.selection.category):
-            self.saveButton.options["invalidMsg"] = "Runs must have a\nnon-empty game and\ncategory."
-        elif not self.entries.leftFrame.isValid():
-            self.saveButton.options["invalidMsg"] = "All split names\nmust be non-empty."
-        elif self.deleteSplitButton["state"] == "disabled":
-            self.saveButton.options["invalidMsg"] = "This run has no splits."
-        elif self.entries.shouldWarn():
-            self.saveWarning.pack(side="bottom",fill="both")
+            self.editor.saveButton.options["invalidMsg"] = "Runs must have a\nnon-empty game and\ncategory."
+        elif not self.editor.entries.leftFrame.isValid():
+            self.editor.saveButton.options["invalidMsg"] = "All split names\nmust be non-empty."
+        elif self.editor.deleteSplitButton["state"] == "disabled":
+            self.editor.saveButton.options["invalidMsg"] = "This run has no splits."
+        elif self.editor.entries.shouldWarn():
+            self.editor.saveWarning.pack(side="bottom",fill="both")
 
-        return self.selection.game and self.selection.category and self.entries.leftFrame.isValid() and (not self.deleteSplitButton["state"] == "disabled")
+        return self.selection.game and self.selection.category and self.editor.entries.leftFrame.isValid() and (not self.editor.deleteSplitButton["state"] == "disabled")
 
     def save(self,retVal):
         if not retVal:
@@ -101,7 +57,7 @@ class SplitEditor(tk.Frame):
         category = self.selection.category
         if self.splits.validPair(game,category):
             category = category + "Copy"
-        csvs = self.entries.generateCsvs()
+        csvs = self.editor.entries.generateCsvs()
         csvs["complete"] = dataManip.newCompleteCsv(csvs["names"])
         fileio.writeCSVs(self.config["baseDir"],game,category,csvs["complete"],csvs["comparisons"])
         self.splits.updateNames(game,category,csvs["names"])
--- Dialogs/SplitEditor.py
+++ Dialogs/SplitEditor.py
@@ -3,6 +3,7 @@ import copy
 from DataClasses import AllSplitNames
 from Dialogs import Popup
 from Components.SplitEditor import EntryGrid
+from Components.SplitEditor import MainEditor
 from Components import SaveButton
 from util import fileio
 from util import dataManip
@@ -10,69 +11,27 @@ from util import dataManip
 class SplitEditor(Popup.Popup):
     def __init__(self,master,callback,state):
         super().__init__(master,callback)
-        self.localComparisons = copy.deepcopy(state.comparesCsv)
         self.state = state
         self.splits = AllSplitNames.Splits()
 
-        self.entries = EntryGrid.EntryGrid(self.window,self.localComparisons)
-        self.entries.pack(side="left")
-
-        self.addSplitButton = tk.Button(self.window, text="Add Split", command=self.addSplit)
-        self.addSplitButton.pack(fill="x")
-
-        self.deleteSplitButton = tk.Button(self.window, text="Delete Split", command=self.deleteSplit)
-        self.deleteSplitButton.pack(fill="x")
-        if not len(self.entries.rows):
-            self.deleteSplitButton["state"] = "disabled"
-
-        self.addComparisonButton = tk.Button(self.window, text="Add Comparison", command=self.addComparison)
-        self.addComparisonButton.pack(fill="x")
-
-        if len(self.entries.comparisons) < 5:
-            enabled = "active"
-        else:
-            enabled = "disabled"
-        self.deleteComparisonButton = tk.Button(self.window, text="Delete Comparison", command=self.deleteComparison, state=enabled)
-        self.deleteComparisonButton.pack(fill="x")
-
-        self.saveButton = SaveButton.SaveButton(self.window, \
-            {"callback": self.save, \
-            "valid": self.validSave, \
-            "invalidMsg": "Runs must have a\nnon-empty game and\ncategory"})
-        self.saveButton.pack(side="bottom",fill="x")
-        self.saveWarning = tk.Label(self.window,text="Warning: some\ncurrent values are\ninvalid. For invalid\nvalues, the most\nrecent valid value\n will be saved.",fg="orange")
-
-    def addSplit(self,event=None):
-        self.entries.addSplit()
-        self.deleteSplitButton["state"] = "active"
-
-    def deleteSplit(self,event=None):
-        self.entries.removeSplit()
-        if not len(self.entries.rows):
-            self.deleteSplitButton["state"] = "disabled"
-
-    def addComparison(self,event=None):
-        self.entries.addComparison()
-        self.deleteComparisonButton["state"] = "active"
-
-    def deleteComparison(self,event=None):
-        self.entries.removeComparison()
-        if len(self.entries.comparisons) <= 5:
-            self.deleteComparisonButton["state"] = "disabled"
+        self.editor = MainEditor.Editor(self.window,state.comparesCsv)
+        self.editor.pack()
+        self.editor.saveButton.options["callback"] = self.save
+        self.editor.saveButton.options["valid"] = self.validSave
 
     def validSave(self):
-        self.saveWarning.pack_forget()
-        if self.deleteSplitButton["state"] == "disabled":
-            self.saveButton.options["invalidMsg"] = "This run has no splits."
-        elif self.entries.shouldWarn():
-            self.saveWarning.pack(side="bottom",fill="both")
+        self.editor.saveWarning.pack_forget()
+        if self.editor.deleteSplitButton["state"] == "disabled":
+            self.editor.saveButton.options["invalidMsg"] = "This run has no splits."
+        elif self.editor.entries.shouldWarn():
+            self.editor.saveWarning.pack(side="bottom",fill="both")
 
-        return not self.deleteSplitButton["state"] == "disabled"
+        return not self.editor.deleteSplitButton["state"] == "disabled"
 
     def save(self,retVal):
         if not retVal:
             return
-        csvs = self.entries.generateCsvs()
-        csvs["complete"] = dataManip.adjustNamesMismatch(csvs["names"],self.state.completeCsv,self.entries.originals)
+        csvs = self.editor.entries.generateCsvs()
+        csvs["complete"] = dataManip.adjustNamesMismatch(csvs["names"],self.state.completeCsv,self.editor.entries.originals)
         fileio.writeCSVs(self.state.config["baseDir"],self.state.game,self.state.category,csvs["complete"],csvs["comparisons"])
         self.splits.updateNames(self.state.game,self.state.category,csvs["names"])
