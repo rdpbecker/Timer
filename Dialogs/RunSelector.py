471698d06247d7839c49225bea9901c7c9d9faf5
Riley Becker <rileydbecker@gmail.com>
2021-06-26 14:56:46 -0400

    Use a RunSelector component in the run selector instead of defining the components explicitly.

--- Dialogs/RunSelector.py
+++ Dialogs/RunSelector.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+from Components import GameSelector
 from Dialogs import BaseDialog
 from util import fileio
 from util import layoutHelper as lh
@@ -18,11 +19,6 @@ class RunSelector(BaseDialog.Dialog):
         super().__init__()
         self.root.title("Choose Run and Layout")
         self.splits = splits
-        self.retVal = {\
-            "game": "",\
-            "category": "",\
-            "layoutName": "System Default"\
-        }
 
         menubar = tk.Menu(self.root, tearoff=False)
         fileMenu = tk.Menu(self.root, tearoff=False)
@@ -40,64 +36,44 @@ class RunSelector(BaseDialog.Dialog):
             i = i + 1
         self.root.configure(bg="black",menu=menubar)
 
-        self.gameVar = tk.StringVar()
-        self.gameVar.trace('w',self.setGame)
-        gameCombo = tk.ttk.Combobox(self.root,values=splits.getGames(),textvariable=self.gameVar)
-        gameLabel = tk.Label(self.root,bg="black",fg="white",text="Game:")
-        gameCombo.grid(row=0,column=4,columnspan=8,sticky="WE")
-        gameLabel.grid(row=0,column=0,columnspan=4,sticky="W")
-
-        self.cateVar = tk.StringVar()
-        self.cateVar.trace('w',self.setCate)
-        self.cateCombo = tk.ttk.Combobox(self.root,values=splits.getCategories(self.game),textvariable=self.cateVar)
-        cateLabel = tk.Label(self.root,bg="black",fg="white",text="Category:")
-        self.cateCombo.grid(row=1,column=4,columnspan=8,sticky="WE")
-        cateLabel.grid(row=1,column=0,columnspan=4,sticky="W")
+        self.selector = GameSelector.Selector(self.root)
+        self.selector.grid(row=0,column=0,columnspan=12,sticky="WE")
 
         self.layoutVar = tk.StringVar()
         self.layoutVar.set("System Default")
         self.layoutVar.trace('w',self.setLayout)
         self.layoutCombo = tk.ttk.Combobox(self.root,values=lh.getLayouts(),textvariable=self.layoutVar)
         layoutLabel = tk.Label(self.root,bg="black",fg="white",text="Layout:")
-        self.layoutCombo.grid(row=2,column=4,columnspan=8,sticky="WE")
-        layoutLabel.grid(row=2,column=0,columnspan=4,sticky="W")
+        self.layoutCombo.grid(row=1,column=4,columnspan=8,sticky="WE")
+        layoutLabel.grid(row=1,column=0,columnspan=4,sticky="W")
 
         confirm = tk.Button(self.root,fg="black",bg="steel blue",text="Confirm Selection",command=self.accept)
-        confirm.grid(row=3,column=0,columnspan=12,sticky="WE")
+        confirm.grid(row=2,column=0,columnspan=12,sticky="WE")
 
     def setRun(self,game,category):
-        self.game = game
-        self.gameVar.set(game)
-        self.category = category
-        self.cateVar.set(category)
-        self.updateCateCombo()
-
-    def setGame(self,*args):
-        self.game = self.gameVar.get()
-        self.updateCateCombo()
-        self.category = ""
-        self.retVal["game"] = self.game
-        self.retVal["category"] = self.category
-        self.retVal["splitNames"] = self.splits.getSplitNames(self.game,self.category)
-
-    def setCate(self,*args):
-        self.category = self.cateVar.get()
-        self.retVal["category"] = self.category
-        self.retVal["splitNames"] = self.splits.getSplitNames(self.game,self.category)
-
-    def updateCateCombo(self):
-        self.cateCombo["values"] = self.splits.getCategories(self.game)
+        self.selector.game = game
+        self.selector.gameVar.set(game)
+        self.selector.category = category
+        self.selector.cateVar.set(category)
+        self.selector.updateCateCombo()
 
     def setLayout(self,*args):
         self.layoutName = self.layoutVar.get()
-        self.retVal["layoutName"] = self.layoutName
 
     def accept(self):
-        if not self.game or not self.category or not self.layoutName:
+        if not self.selector.game or not self.selector.category or not self.layoutName:
             error = tk.Label(self.root,bg="black",fg="white",text="A game and category must both be selected.")
-            error.grid(row=4,column=0,columnspan=12,sticky="WE")
+            error.grid(row=3,column=0,columnspan=12,sticky="WE")
             return
+        self.setReturn()
+        self.retVal["exitCode"] = "accept"
         self.root.destroy()
 
     def finish(self):
         self.accept()
+
+    def setReturn(self):
+        self.retVal["game"] = self.selector.game
+        self.retVal["category"] = self.selector.category
+        self.retVal["splitNames"] = self.selector.splits.getSplitNames(self.selector.game,self.selector.category)
+        self.retVal["layoutName"] = self.layoutName
