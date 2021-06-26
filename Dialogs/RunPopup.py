e3d01c7cc9293b38ba5b29f52ce93aeb8db75cd6
Riley Becker <rileydbecker@gmail.com>
2021-06-26 16:41:28 -0400

    Add a game selector to the run popup instead of adding the combo boxes explicitly (last commit was the dialog, not the popup).

--- Dialogs/RunPopup.py
+++ Dialogs/RunPopup.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+from Components import GameSelector
 from DataClasses import AllSplitNames
 from Dialogs import Popup
 from util import layoutHelper as lh
@@ -18,12 +19,7 @@ class RunPopup(Popup.Popup):
         self.splits = AllSplitNames.Splits()
         self.game = session.game
         self.category = session.category
-        self.retVal = {\
-            "game": session.game,\
-            "category": session.category\
-        }
 
-    def show(self):
         self.window.configure(bg="black")
         self.window.title("Choose Run")
 
@@ -43,39 +39,16 @@ class RunPopup(Popup.Popup):
             i = i + 1
         self.window.configure(bg="black",menu=menubar)
 
-        self.gameVar = tk.StringVar()
-        self.gameVar.set(self.session.game)
-        self.gameVar.trace('w',self.setGame)
-        gameCombo = tk.ttk.Combobox(self.window,values=self.splits.getGames(),textvariable=self.gameVar)
-        gameLabel = tk.Label(self.window,bg="black",fg="white",text="Game:")
-        gameCombo.grid(row=0,column=4,columnspan=8,sticky="WE")
-        gameLabel.grid(row=0,column=0,columnspan=4,sticky="W")
-
-        self.cateVar = tk.StringVar()
-        self.cateVar.set(self.session.category)
-        self.cateVar.trace('w',self.setCate)
-        self.cateCombo = tk.ttk.Combobox(self.window,values=self.splits.getCategories(self.game),textvariable=self.cateVar)
-        cateLabel = tk.Label(self.window,bg="black",fg="white",text="Category:")
-        self.cateCombo.grid(row=1,column=4,columnspan=8,sticky="WE")
-        cateLabel.grid(row=1,column=0,columnspan=4,sticky="W")
+        self.selector = GameSelector.Selector(self.window)
+        self.selector.pack()
 
         confirm = tk.Button(self.window,fg="black",bg="steel blue",text="Confirm Selection",command=self.accept)
-        confirm.grid(row=3,column=0,columnspan=12,sticky="WE")
+        confirm.pack(fill="x")
 
     def setRun(self,game,category):
-        self.gameVar.set(game)
-        self.cateVar.set(category)
+        self.selector.gameVar.set(game)
+        self.selector.cateVar.set(category)
 
-    def setGame(self,*args):
-        self.game = self.gameVar.get()
-        self.updateCateCombo()
-        self.category = ""
-        self.retVal["game"] = self.game
-        self.retVal["category"] = self.category
-
-    def setCate(self,*args):
-        self.category = self.cateVar.get()
-        self.retVal["category"] = self.category
-
-    def updateCateCombo(self):
-        self.cateCombo["values"] = self.splits.getCategories(self.game)
+    def setReturn(self):
+        self.retVal["game"] = self.selector.game
+        self.retVal["category"] = self.selector.category
